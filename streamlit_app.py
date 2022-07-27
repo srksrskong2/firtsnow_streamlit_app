cf10c21caa15cc1ad442f4a80fbe08cd11bb249c
srkrsrkrong2 <109849886+srksrskong2@users.noreply.github.com>
2022-07-27 21:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+requirements.txt
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
