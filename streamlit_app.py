cb2c412c647f68b55d6d469f2988a6a8aabc0b10
srkrsrkrong2 <109849886+srksrskong2@users.noreply.github.com>
2022-07-28 09:46:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ else:
        streamlit.dataframe(fruityvice_normalized)
  except URL Error as e:
           streamlit.error()
+          
+ fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
                                     
 
 
