572cbfee85c01537420931b41a923884a12092bc
srkrsrkrong2 <109849886+srksrskong2@users.noreply.github.com>
2022-07-28 08:32:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ streamlit.write('Thanks for adding fruit', 'add my fruit')
 
 
 my_cur = my_cnx.cursor()
-my_cur.execute(" inert into fruit_load_list values ('from streamlit')")
+my_cur1 = my_cnx.cursor()
+my_cur1.execute("inert into fruit_load_list values ('from streamlit')")
 my_cur.execute(" select * from fruit_load_list")
 
 
