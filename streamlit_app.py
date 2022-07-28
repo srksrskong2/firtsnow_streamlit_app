6b3398c96df65f57b6bf6f4dd2c446fa60b95e8e
srkrsrkrong2 <109849886+srksrskong2@users.noreply.github.com>
2022-07-28 08:04:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,7 @@ my_cur = my_cnx.cursor()
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 
