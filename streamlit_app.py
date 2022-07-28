b8ff8678bfbdecf13ddb0b2a85e011d8dea3f737
srkrsrkrong2 <109849886+srksrskong2@users.noreply.github.com>
2022-07-28 09:23:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 import pandas
 import requests
-
-
-
 import snowflake.connector
 
 
@@ -48,7 +45,7 @@ my_cur.execute(" select * from fruit_load_list")
 
 my_data_rows= my_cur.fetchall()
 
-my_cur.execute("inert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("inert into fruit_load_list values ('from streamlit')")
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 fruit_choice1 = streamlit.text_input('What fruit would you like add?','jackfruit')
